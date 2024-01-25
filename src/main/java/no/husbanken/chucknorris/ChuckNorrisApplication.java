3a8da552069a101486956f699f078b376eddeea7
hoid <eirik.eidsa@husbanken.no>
2024-01-25 15:10:53 +0100

    log filter, clean up

--- src/main/java/no/husbanken/chucknorris/ChuckNorrisApplication.java
+++ src/main/java/no/husbanken/chucknorris/ChuckNorrisApplication.java
@@ -3,6 +3,8 @@ package no.husbanken.chucknorris;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @EnableFeignClients
 @SpringBootApplication
@@ -10,6 +12,16 @@ public class ChuckNorrisApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChuckNorrisApplication.class, args);
+	}
+
+	@Bean
+	public CommonsRequestLoggingFilter logFilter() {
+		CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+		filter.setIncludeQueryString(true);
+		filter.setIncludePayload(false);
+		filter.setMaxPayloadLength(10000);
+		filter.setIncludeHeaders(false);
+		return filter;
 	}
 
 }
--- src/main/java/no/husbanken/chucknorris/ChuckNorrisService.java
+++ src/main/java/no/husbanken/chucknorris/ChuckNorrisService.java
@@ -3,10 +3,10 @@ package no.husbanken.chucknorris;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.Random;
 
@@ -47,7 +47,6 @@ public class ChuckNorrisService {
 
     private final static ObjectMapper objectMapper = new ObjectMapper();
 
-    @SneakyThrows
     public Joke getRandomChuckNorrisJoke() {
         List<Joke> listOfJokes = JOKES;
 
@@ -55,8 +54,12 @@ public class ChuckNorrisService {
             File file = new File(chuckProperties.getJokeLocation());
 
             if (file.exists()) {
-                listOfJokes = objectMapper.readValue(file, new TypeReference<>() {
-                });
+                try {
+                    listOfJokes = objectMapper.readValue(file, new TypeReference<>() {
+                    });
+                } catch (IOException e) {
+                    throw new RuntimeException("Failed to parse json", e);
+                }
 
             } else {
                 throw new RuntimeException("File with jokes does not exist!");
